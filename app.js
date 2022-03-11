330a04924e8675ba29e9113c7bc0d6c57faeb58d
lavindude <lavindude@gmail.com>
2022-03-11 14:36:41 -0800

    got some more code transferred from old API to new API

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 const port = 4000
 
-const connectedPlayers = []
-const lobbies = []
+// const connectedPlayers = []
+// const lobbies = []
+
+const connectedPlayers = [{id: 1, positionX: 0, positionY: 0, positionZ: 0},
+                           {id: 2, positionX: 0, positionY: 0, positionZ: 0}
+                         ]
+const lobbies = [{id: 1, numOfPlayers: 4, lobbyPlayers: [connectedPlayers[0], connectedPlayers[1]]}]
 
 const getIndex = (playerId, lobbyId) => {
     const curLobbyArray = lobbies[lobbyId-1].lobbyPlayers
@@ -20,13 +25,21 @@ app.get('/printHello', function (req, res) {
 
 // Queries:
 app.get(`/getLobbyPlayers`, function (req, res) {
-    const lobbyId = req.query.lobbyId
-    res.send(lobbyId)
+    try {
+        const lobbyId = req.query.lobbyId
+        res.send(lobbies[lobbyId-1].lobbyPlayers)
+    }
+
+    catch {
+        res.send("Error")
+    }
 })
 
 app.get('/getConnectedPlayers', function (req, res) {
-    
+    res.send(connectedPlayers)
 })
+
+//maybe make a simple query to test multiplayer
 
 app.get('/getLobbies', function (req, res) {
     
